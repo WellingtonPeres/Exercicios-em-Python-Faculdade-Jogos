0262e0f9352b4a161c3708355e1f974f41ee18da
Wellington Peres Vasconcelos <wellingtonpv.100@gmail.com>
2021-04-21 18:29:14 -0300

    Acrescentar duas unidades no valor

---
+++ Acrescentando mais unidade a um valor.py
@@ -0,0 +1,10 @@
+""" Desenvolva um programa em Python que recebe um valor
+inteiro, acrescente duas unidades a este, e exiba
+o resultado desta manipulação."""
+numero = 8
+acrescentar = numero + 2
+print("A soma entre os dois valores", acrescentar)
+
+numero2 = int(input())
+acrescentar2 = 2
+print(f"A soma entre {numero2} e {acrescentar2} é: {numero2 + acrescentar2}")
