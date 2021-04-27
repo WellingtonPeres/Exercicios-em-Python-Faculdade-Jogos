4437ac67dde95c3a56cf9de27afec1709d04aac5
Wellington Peres Vasconcelos <wellingtonpv.100@gmail.com>
2021-04-27 17:19:34 -0300

    multiplicação de dois inteiros quaisquer.

---
+++ Ex 003 - Multiplicação de dois números inteiros.py
@@ -0,0 +1,10 @@
+''' Desenvolva um programa em Python para representar um algoritmo que
+efetue a multiplicação de dois inteiros quaisquer.'''
+
+print("Digite o primeiro número: ")
+numero1 = int(input())
+print("Digite o segundo número: ")
+numero2 = int(input())
+
+resultadoMultiplicacao = numero1 * numero2
+print(f"A multiplicação entre {numero1} e {numero2} é {resultadoMultiplicacao}")
