0cf8772fe6e5f1c13f2c327193afb8284f492bc8
Wellington Peres Vasconcelos <wellingtonpv.100@gmail.com>
2021-05-03 06:39:59 -0300

    área do círculo

---
+++ Ex 005 - Área do círculo.py
@@ -0,0 +1,7 @@
+''' Desenvolva um programa em C# que receba
+o valor de um raio e retorne a área do círculo.'''
+
+print("Digite um valor de um raio de sua preferência: ")
+raio = float(input())
+
+print(f"A área do círculo é {3.14 * (raio * raio)}")
